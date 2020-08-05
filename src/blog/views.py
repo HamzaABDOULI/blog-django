22cc7095edaf233c0d68dd0b97a1d4273f5d3120
HamzaABDOULI <abdouli.hamza568@gamil.com>
2020-08-05 21:09:12 +0100

    Make base  and index pages

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+    {
+        'title':'The first post',
+        'content':' first text ',
+        'post_date':'05/8/2020',
+        'auther':'Hamza ABDOULI' ,
+    },
+    {
+        'title':'The second post',
+        'content':' text 2 ',
+        'post_date':'04/7/2020',
+        'auther':'Ghassen ABDOULI' ,
+    },
+    {
+        'title':'The third post',
+        'content':' text ',
+        'post_date':'03/7/2020',
+        'auther':'Iheb ABDOULI' ,
+    },
+    {
+        'title':'The fourth post',
+        'content':' text ',
+        'post_date':'02/7/2020',
+        'auther':'Iheb ABDOULI' ,
+    },
+    {
+        'title':'The fifth post',
+        'content':'last text ',
+        'post_date':'01/6/2020',
+        'auther':'Hamza ABDOULI' ,
+    }
+]
 
 def home(request):
-    return render(request, 'blog/index.html', {'title':'Home'})
+    
+    context = { 
+        'title': 'Home',
+        'pos': posts 
+        }
+    return render(request, 'blog/index.html', context )
